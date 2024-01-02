b563e6686d8a178dd6d4098bea8ac7c2c831872e
Yeamin-Bhuiyan <141159594+Yeamin-Bhuiyan@users.noreply.github.com>
2024-01-02 06:29:34 +0600

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/A. Say Hello With C++.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = acos(-1);
+typedef long long ll;
+
+#define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+int main()
+{
+	optimize();
+    string s ;
+    cin >> s ;
+    cout << "Hello, " << s << endl ;
+
+	return 0;
+}
