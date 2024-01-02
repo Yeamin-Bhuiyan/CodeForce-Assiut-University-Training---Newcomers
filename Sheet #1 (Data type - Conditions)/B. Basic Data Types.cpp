91f857fc9ca448f20763dc35594905ac732e7d70
Yeamin-Bhuiyan <141159594+Yeamin-Bhuiyan@users.noreply.github.com>
2024-01-02 06:29:50 +0600

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const double PI = acos(-1);
+typedef long long ll;
+
+#define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+int main()
+{
+	optimize();
+	fraction();
+
+    int a ;
+    ll b ;
+    char c ;
+    float d ;
+    double e ;
+    cin >> a >> b >> c >> d >> e ;
+    cout << a << endl << b << endl << c << endl << d << endl << e << endl ;
+
+	return 0;
+}
